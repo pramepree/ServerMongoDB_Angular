fcfcc348c41147b4c5bbc22e8284bc990595b598
pramepree <pramepree159@gmail.com>
2024-05-29 15:40:09 +0700

    Update server.js

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.post('/articles', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
