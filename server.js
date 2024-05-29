a98ed1f0c0c8a6c5555c605b3925abbb5935941a
pramepree <pramepree159@gmail.com>
2024-05-29 15:58:55 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
