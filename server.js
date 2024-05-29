c84e8a561796b30d9b235e2be729217d80f74cc9
pramepree <pramepree159@gmail.com>
2024-05-29 10:07:57 +0700

    update version

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb+srv://pramepreejobe:hylZa0TEmLlrb6jG@cluster0.znplgct.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://pramepreejobe:hylZa0TEmLlrb6jG@cluster0.znplgct.mongodb.net/<your-database>?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  ssl: true
+});
 
 const ArticleSchema = new mongoose.Schema({
   title: String,
