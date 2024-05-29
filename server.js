4d150c9305f444caa2fe58d0ff5fbfb73c3cb6a9
pramepree <pramepree159@gmail.com>
2024-05-29 11:15:21 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,51 +1,11 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config(); // Load environment variables from .env file
-
 const app = express();
-app.use(express.json());
-app.use(cors());
-
-const connectionString = process.env.MONGODB_URI;
-
-mongoose.connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  tls: true // Enabling TLS explicitly if needed
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch(err => {
-  console.error('Error connecting to MongoDB:', err.message);
-});
-
-const ArticleSchema = new mongoose.Schema({
-  title: String,
-  content: String
-});
-
-const Article = mongoose.model('articles', ArticleSchema);
-
-app.get('/', async (req, res) => {
-  try {
-    const articles = await Article.find();
-    res.json(articles); // Return the articles as JSON
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
-
-app.post('/articles', async (req, res) => {
-  try {
-    const newArticle = req.body;
-    const createdArticle = await Article.create(newArticle);
-    res.status(201).json(createdArticle);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
-
 const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('ssss');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
