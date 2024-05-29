72d1d015fc0ba8e5267ab45e72c17d2731d7180d
pramepree <pramepree159@gmail.com>
2024-05-29 17:03:21 +0700

    up cache

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const NodeCache = require('node-cache');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
@@ -6,6 +7,8 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const cache = new NodeCache();
 
 const connectionString = process.env.CONNECTION_STRING;
 
@@ -27,11 +30,17 @@ const ArticleSchema = new mongoose.Schema({
 const Article = mongoose.model('articles', ArticleSchema);
 
 app.get('/articles', async (req, res) => {
-  try {
-    const articles = await Article.find();
-    res.json(articles);
-  } catch (err) {
-    res.status(500).send(err);
+  const cachedArticles = cache.get('articles');
+  if (cachedArticles) {
+    res.json(cachedArticles);
+  } else {
+    try {
+      const articles = await Article.find();
+      cache.set('articles', articles, 3600); // cache for 1 hour
+      res.json(articles);
+    } catch (err) {
+      res.status(500).send(err);
+    }
   }
 });
 
@@ -39,6 +48,7 @@ app.post('/articles', async (req, res) => {
   try {
     const newArticle = req.body;
     const createdArticle = await Article.create(newArticle);
+    cache.del('articles'); // invalidate cache
     res.status(201).json(createdArticle);
   } catch (err) {
     res.status(500).send(err);
